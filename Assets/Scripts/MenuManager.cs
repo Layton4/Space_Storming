d5869fd5925e8e5e7be942f82858d1fec731eab3
Layton4 <73424805+Layton4@users.noreply.github.com>
2023-01-23 13:44:02 +0100

    MenuProcess

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject[] arrowsSprites;
+    
+    public void ShowSelectArrow(GameObject arrowSprite)
+    {
+        foreach(GameObject a in arrowsSprites) { a.SetActive(false); }
+        arrowSprite.SetActive(true);
+    }
+
+    public void HideSelectArrow(GameObject arrowSprite)
+    {
+        foreach(GameObject a in arrowsSprites) { a.SetActive(false); }
+    }
+
+    public void ExitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+
+    }
+
+
+}
