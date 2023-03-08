e46dfbf9d4b7770a097eb0e4f915e4466ef2f6d0
Layton4 <Javierete4774@gmail.com>
2023-03-08 22:53:35 +0100

    Limpiando al límite

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,8 +6,6 @@ using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
-    public Toggle[] autosafeToggle;
-
     public GameObject[] arrowsSprites;
 
     public Button continueButton;
@@ -23,7 +21,7 @@ public class MenuManager : MonoBehaviour
 
     private void Awake()
     {
-        SceneFlowScript = FindObjectOfType<SceneFlow>();
+        SceneFlowScript = FindObjectOfType<SceneFlow>(); //We save in this var the ScenFlow that will allow us travel between scenes from the menu
     }
     private void Start()
     {
@@ -40,7 +38,7 @@ public class MenuManager : MonoBehaviour
         }
     }
 
-    public void NextAutoselect(Button button)
+    public void NextAutoselect(Button button) //To let the player navigate through the menu with the arrows when we change of panel we select the first selected button
     {
         button.Select();
     }
@@ -48,23 +46,22 @@ public class MenuManager : MonoBehaviour
     #region SelectionArrows
     public void ShowSelectArrow(GameObject arrowSprite)
     {
-        foreach(GameObject a in arrowsSprites) { a.SetActive(false); }
+        foreach (GameObject a in arrowsSprites) { a.SetActive(false); }
         arrowSprite.SetActive(true);
     }
 
     public void HideSelectArrow(GameObject arrowSprite)
     {
-        foreach(GameObject a in arrowsSprites) { a.SetActive(false); }
+        foreach (GameObject a in arrowsSprites) { a.SetActive(false); }
     }
 
     #endregion
 
     #region MenuButtons
+
     public void ExitGame()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
-        Application.Quit();
-
+        Application.Quit(); //Exit the aplication, only in the Build
     }
     public void OpenOptionPanel(GameObject panel)
     {
@@ -74,10 +71,9 @@ public class MenuManager : MonoBehaviour
 
     public void backButton(GameObject closedPanel)
     {
-        
-        OptionsPanelAnimator.SetBool("isActivated",false); //Activated the animation of closing the panel
-        DataPersistance.SaveForFutureGames();
-        //StartCoroutine(WaitAndOff(1.3f, closedPanel));
+
+        OptionsPanelAnimator.SetBool("isActivated", false); //Activated the animation of closing the panel
+        DataPersistance.SaveForFutureGames(); //We save, just in case the user settings even if the slider save it each time has a value changed
     }
 
     public void NewGameButton()
@@ -85,10 +81,8 @@ public class MenuManager : MonoBehaviour
         NewGamePersistance();
         SpacePanel.SetActive(true);
         SpacePanel.GetComponent<Animator>().SetBool("active", true);
-        SceneFlowScript.GoToNewGame();
-
+        StartCoroutine(SceneFlowScript.GoToScene("NewGame", 1.2f));
     }
-
 
     public void ContinueButton()
     {
@@ -99,98 +93,109 @@ public class MenuManager : MonoBehaviour
 
     #endregion
 
-    #region AutosafeConfiguration
-    public void AutoSaveOption(bool value) //When the value of the toggle changes we want to safe in datapersistence if the OnToggle is turn on or off.
+   
+     public void LoadUserSettings() //When we arrive to the mainMenu the autosafe options we mark in the previous game is On.
     {
-        DataPersistance.autosaveToggle = value ? 1 : 0;
-    }
-
-    public void LoadUserSettings() //When we arrive to the mainMenu the autosafe options we mark in the previous game is On.
-    {
-        autosafeToggle[PlayerPrefs.GetInt("Autosave_Toggle",1)].isOn = true;
         DataPersistance.hasPlayed = PlayerPrefs.GetInt("Has_Played", 0);
 
-        DataPersistance.piecesRemain = PlayerPrefs.GetInt("PiecesRemain", 4);
+        #region Items & Inventory
+        DataPersistance.item1 = PlayerPrefs.GetInt("Item1", 0);
+        DataPersistance.item2 = PlayerPrefs.GetInt("Item2", 0);
+        DataPersistance.item3 = PlayerPrefs.GetInt("Item3", 0);
+        DataPersistance.item4 = PlayerPrefs.GetInt("Item4", 0);
 
         DataPersistance.inventory1 = PlayerPrefs.GetInt("Inventory1", 0);
         DataPersistance.inventory2 = PlayerPrefs.GetInt("Inventory2", 0);
         DataPersistance.inventory3 = PlayerPrefs.GetInt("Inventory3", 0);
         DataPersistance.inventory4 = PlayerPrefs.GetInt("Inventory4", 0);
         DataPersistance.inventory5 = PlayerPrefs.GetInt("Inventory5", 0);
+        #endregion
 
+        #region pieces obtained
         DataPersistance.piece1 = PlayerPrefs.GetInt("Piece1", 0);
         DataPersistance.piece2 = PlayerPrefs.GetInt("Piece2", 0);
         DataPersistance.piece3 = PlayerPrefs.GetInt("Piece3", 0);
         DataPersistance.piece4 = PlayerPrefs.GetInt("Piece4", 0);
+        DataPersistance.piecesRemain = PlayerPrefs.GetInt("PiecesRemain", 4);
+        #endregion
 
-        DataPersistance.item1 = PlayerPrefs.GetInt("Item1", 0);
-        DataPersistance.item2 = PlayerPrefs.GetInt("Item2", 0);
-        DataPersistance.item3 = PlayerPrefs.GetInt("Item3", 0);
-        DataPersistance.item4 = PlayerPrefs.GetInt("Item4", 0);
-
-        DataPersistance.Dialogue1Done = PlayerPrefs.GetInt("Dialogue1Done", 0);
-        DataPersistance.Dialogue3Done = PlayerPrefs.GetInt("Dialogue3Done", 0);
-
-        DataPersistance.playerXPos = PlayerPrefs.GetFloat("PlayerXPos", 0);
-        DataPersistance.playerYPos = PlayerPrefs.GetFloat("PlayerYPos", 0);
-
-
-        DataPersistance.DialoguePiecesDone = PlayerPrefs.GetInt("PiecesDialogueDone", 0);
-
+        #region Doors Openned
         DataPersistance.door1 = PlayerPrefs.GetInt("Door1", 0);
         DataPersistance.door2 = PlayerPrefs.GetInt("Door2", 0);
         DataPersistance.door3 = PlayerPrefs.GetInt("Door3", 0);
         DataPersistance.door4 = PlayerPrefs.GetInt("Door4", 0);
         DataPersistance.door5 = PlayerPrefs.GetInt("Door5", 0);
         DataPersistance.door6 = PlayerPrefs.GetInt("Door6", 0);
+        #endregion
+
+        #region DialoguesDone
+        DataPersistance.Dialogue1Done = PlayerPrefs.GetInt("Dialogue1Done", 0);
+        DataPersistance.Dialogue3Done = PlayerPrefs.GetInt("Dialogue3Done", 0);
+        DataPersistance.DialoguePiecesDone = PlayerPrefs.GetInt("PiecesDialogueDone", 0);
+        #endregion
+
+        #region Player Last saved Position
+        DataPersistance.playerXPos = PlayerPrefs.GetFloat("PlayerXPos", 0);
+        DataPersistance.playerYPos = PlayerPrefs.GetFloat("PlayerYPos", 0);
+        #endregion
+
     }
 
     public void NewGamePersistance()
     {
-        autosafeToggle[PlayerPrefs.GetInt("Autosave_Toggle", 1)].isOn = true;
-        DataPersistance.hasPlayed = PlayerPrefs.GetInt("Has_Played", 0);
-        DataPersistance.piecesRemain = 4;
+        //When we hit the new Game Button all the data of data persistence must be reset, to become a normal game,
 
+        DataPersistance.hasPlayed = PlayerPrefs.GetInt("Has_Played", 0);
+
+        #region Items & inventory
         DataPersistance.inventory1 = 0;
         DataPersistance.inventory2 = 0;
         DataPersistance.inventory3 = 0;
         DataPersistance.inventory4 = 0;
         DataPersistance.inventory5 = 0;
 
-        DataPersistance.piece1 = 0;
-        DataPersistance.piece2 = 0;
-        DataPersistance.piece3 = 0;
-        DataPersistance.piece4 = 0;
-
         DataPersistance.item1 = 0;
         DataPersistance.item2 = 0;
         DataPersistance.item3 = 0;
         DataPersistance.item4 = 0;
+        #endregion
 
-        DataPersistance.Dialogue1Done = 0;
-        DataPersistance.Dialogue3Done = 0;
+        #region  PiecesObtained
+        DataPersistance.piece1 = 0;
+        DataPersistance.piece2 = 0;
+        DataPersistance.piece3 = 0;
+        DataPersistance.piece4 = 0;
+        DataPersistance.piecesRemain = 4;
+        #endregion
 
-        DataPersistance.playerXPos = 0f;
-        DataPersistance.playerYPos = 0;
-
-        DataPersistance.DialoguePiecesDone = 0;
-
+        #region doors
         DataPersistance.door1 = 0;
         DataPersistance.door2 = 0;
         DataPersistance.door3 = 0;
         DataPersistance.door4 = 0;
         DataPersistance.door5 = 0;
         DataPersistance.door6 = 0;
-    }
-    #endregion
+        #endregion
 
-    #region Corroutines
+        #region dialoguesDone
+        DataPersistance.Dialogue1Done = 0;
+        DataPersistance.Dialogue3Done = 0;
+        DataPersistance.DialoguePiecesDone = 0;
+        #endregion
+
+        #region Player Last saved position
+        DataPersistance.playerXPos = 0f;
+        DataPersistance.playerYPos = 0;
+        #endregion
+    }
+
+
+
     IEnumerator WaitAndOff(float timeToWait, GameObject s)
     {
         yield return new WaitForSeconds(timeToWait);
         s.SetActive(false);
     }
 
-    #endregion
 
 }
